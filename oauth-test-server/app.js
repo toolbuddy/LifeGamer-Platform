8012e7942d137c812cc89f913ef17e752a35a71d
You-Cheng Liang <liangyc39@gmail.com>
2018-02-04 17:19:50 +0800

    Add gitlab api access for oauth-test-server
    npm package `node-gitlab-api` is used for doing this

--- oauth-test-server/app.js
+++ oauth-test-server/app.js
@@ -16,46 +16,61 @@ const oauth2 = simpleOauthModule.create({
 
 // Authorization uri definition
 const authorizationUri = oauth2.authorizationCode.authorizeURL({
-  redirect_uri: 'https://hmkrl.com/callback',
-  scopes: 'read_user+api',
-  state: '8787'
+    redirect_uri: 'https://hmkrl.com/callback',
+    scopes: 'read_user+api',
+    state: '8787'
 });
 
 // Initial page redirecting to Github
 app.get('/auth', (req, res) => {
-  console.log(authorizationUri);
-  res.redirect(authorizationUri);
+    console.log(authorizationUri);
+    res.redirect(authorizationUri);
 });
 
 // Callback service parsing the authorization token and asking for the access token
 app.get('/callback', (req, res) => {
-  const code = req.query.code;
-  const options = {
-    code: req.query.code,
-    grant_type: 'authorization_code',
-    redirect_uri: 'https://hmkrl.com/callback'
-  };
+    const code = req.query.code;
+    const options = {
+        code: req.query.code,
+        grant_type: 'authorization_code',
+        redirect_uri: 'https://hmkrl.com/callback'
+    };
 
-  oauth2.authorizationCode.getToken(options, (error, result) => {
-    if (error) {
-      console.error('Access Token Error', error.message);
-      return res.json('Authentication failed');
-    }
 
-    console.log('The resulting token: ', result);
-    const token = oauth2.accessToken.create(result);
+    oauth2.authorizationCode.getToken(options, (error, result) => {
+        if (error) {
+            console.error('Access Token Error', error.message);
+            return res.json('Authentication failed');
+        }
 
-    return res
-      .status(200)
-      .json(token);
-  });
+        console.log('The resulting token: ', result);
+        const token = oauth2.accessToken.create(result);
+
+        console.log(token['token']['access_token'])
+
+            const GitlabAPI = require('node-gitlab-api')({
+                url:   'https://hmkrl.com/gitlab', // Defaults to http://gitlab.com
+                    oauthToken: token['token']['access_token']
+            })
+
+        var images = '<html>';
+
+        GitlabAPI.users.all()
+            .then((users) => {
+                users.forEach((user) => {
+                    images += '<img src="' + user['avatar_url'] + '"/><br>\n';
+                })
+                res.end(images + '</html>')
+            })
+    });
+
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello<br><a href="/auth">Log in with Github</a>');
+    res.send('Hello<br><a href="/auth">Log in with Github</a>');
 });
 
 app.listen(3000, () => {
-  console.log('Express server started on port 3000'); // eslint-disable-line
+    console.log('Express server started on port 3000'); // eslint-disable-line
 });
 
