3eadf42ad1d167c49ac7a03fd69120ea6d38073a
laxative <laxative0612@gmail.com>
2018-02-05 21:22:35 +0800

    Add function of auth operation
    when user access the website, would check cookie exist or not. If not
    exist, it would redirect to <host>/auth for backend process.

--- Homepage/src/App.vue
+++ Homepage/src/App.vue
@@ -12,7 +12,42 @@ import Menu from "./components/Menu";
 
 export default {
   name: "App",
-  components: { Menu, HeaderBar }
+  components: { Menu, HeaderBar },
+  created() {
+    let token = this.getCookie("token");
+    if (token == "") {
+      window.location.href = "https://hmkrl.com/auth";
+    } else {
+      console.log(token);
+    }
+  },
+  methods: {
+    /* cookie operating function */
+    setCookie: function(cname, cvalue, exMin) {
+      var d = new Date();
+      /* cookie exist time */
+      d.setTime(d.getTime() + exMin * 60 * 1000); // cookie exist exMin minutes
+      var expires = "expires=" + d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    },
+    getCookie: function(cname) {
+      var name = cname + "=";
+      var ca = document.cookie.split(";");
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == " ") {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    },
+    deleteCookie: function(cname) {
+      document.cookie = `${cname}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    }
+  }
 };
 </script>
 
