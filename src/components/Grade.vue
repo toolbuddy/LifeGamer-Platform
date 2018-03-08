ed2ffdd1729b3bb7bec38041faa7b1088562736c
Yao-Hsien Hsieh <laxative0612@gmail.com>
2018-03-08 21:57:30 +0800

    Make time format

--- src/components/Grade.vue
+++ src/components/Grade.vue
@@ -17,12 +17,59 @@
 <script>
 const config = require('../../config/config')
 
+/* Date format */
+Date.prototype.pattern = function (fmt) {
+  var o = {
+    'M+': this.getMonth() + 1, // month
+    'd+': this.getDate(), // dat
+    'h+': this.getHours() % 12 === 0 ? 12 : this.getHours() % 12, // hour
+    'H+': this.getHours(), // hour
+    'm+': this.getMinutes(), // minute
+    's+': this.getSeconds(), // second
+    'q+': Math.floor((this.getMonth() + 3) / 3), // season
+    S: this.getMilliseconds() // millisecond
+  }
+  var week = {
+    '0': '/u65e5',
+    '1': '/u4e00',
+    '2': '/u4e8c',
+    '3': '/u4e09',
+    '4': '/u56db',
+    '5': '/u4e94',
+    '6': '/u516d'
+  }
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(
+      RegExp.$1,
+      (this.getFullYear() + '').substr(4 - RegExp.$1.length)
+    )
+  }
+  if (/(E+)/.test(fmt)) {
+    fmt = fmt.replace(
+      RegExp.$1,
+      (RegExp.$1.length > 1
+        ? RegExp.$1.length > 2 ? '/u661f/u671f' : '/u5468'
+        : '') + week[this.getDay() + '']
+    )
+  }
+  for (var k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) {
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length === 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length)
+      )
+    }
+  }
+  return fmt
+}
+
 export default {
   name: 'grade',
   data: function () {
     return {
       token: null,
       userdata: null,
+      branch: null,
       commits: null,
       loaded: false
     }
@@ -51,8 +98,16 @@ export default {
         )
         .then(response => {
           this.commits = JSON.parse(response.bodyText)
-          console.log('commits: ' + this.commits['dev'])
+          this.branch = Object.keys(this.commits)
           this.loaded = true
+        })
+        .then(() => {
+          this.branch.forEach(list => {
+            this.commits[list].forEach(item => {
+              let datetime = new Date(item.committed_date)
+              item.committed_date = datetime.pattern('yyyy-MM-dd hh:mm:ss')
+            })
+          })
         })
     }
   }
@@ -89,13 +144,22 @@ export default {
 .commit-item {
   float: left;
   margin: 0 10px;
+  height: 100%;
 }
 
 .commit-title {
-  width: 40%;
+  width: 50%;
 }
 
 .commit-time {
   width: 20%;
+}
+
+.commit-shortid {
+  width: 15%;
+}
+
+.commit-button {
+  width: 15%;
 }
 </style>
