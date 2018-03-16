96ef2f1c850efa5eca74cb84f6530b17169e5f7c
Yao-Hsien Hsieh <laxative0612@gmail.com>
2018-03-16 23:49:32 +0800

    Change pipeline jobs data type, read more easily

--- src/components/Grade.vue
+++ src/components/Grade.vue
@@ -2,6 +2,8 @@
 <template>
     <section class="section-wrapper">
       <div v-if='this.stage === "finish"'>
+        <!-- The template here showing all pipelines status -->
+        <!-- start -->
         <div class="pipelines-row pipelines-header-row">
           <div class="pipelines-item pipelines-id">Pipeline ID</div>
           <div class="pipelines-item pipelines-commit-id">Commit SHA</div>
@@ -15,9 +17,20 @@
             <div class="pipelines-item pipelines-score" v-html="totalScore(pipeline.jobs)"></div>
             <div class="pipelines-item pipelines-button click-button">Detail</div>
           </div>
+          <!-- end -->
+          <!-- The template here showing jobs detail inside a pipeline -->
+          <!-- start -->
           <div class="pipelines-details">
-
+            <!-- unordered list show all stage -->
+            <ul v-for="(stage, index) in pipeline.jobs.stages" :key="index">
+              {{ stage }}
+              <!-- list show all jobs in stage -->
+              <li v-for="(job, index) in pipeline.jobs[stage]" :key="index">
+                {{ job }}
+              </li>
+            </ul>
           </div>
+          <!-- end -->
         </div>
       </div>
     </section>
@@ -60,22 +73,70 @@ export default {
         )
         .then(response => {
           this.pipelinejobs = JSON.parse(response.bodyText)
-          this.stage = 'finish'
         })
+        .then(async () => {
+          await this.stageSorting()
+          this.stage = 'finish'
+          console.log(this.pipelinejobs[0].jobs)
+        })
+    },
+    stageSorting: function () {
+      return new Promise(resolve => {
+        /* pipeline jobs json format
+         *
+         * {
+         *    'stage': [stage1, stage2, ...],
+         *    'pipelineStatus': pipelineStatus,
+         *    'stage1': [{'name': jobName, 'status': jobStatus }],
+         *    'stage2': [{'name': jobName, 'status': jobStatus }],
+         *     ...
+         * }
+         *
+        */
+        this.pipelinejobs.forEach(item => {
+          let jobSort = {}
+          /* set stages array */
+          jobSort['stages'] = []
+          item.jobs.forEach(job => {
+            /* check json has key or not */
+            if (jobSort.hasOwnProperty(job.stage)) {
+              let jobData = {
+                status: job.status,
+                name: job.name
+              }
+              jobSort[job.stage].push(jobData)
+            } else {
+              jobSort['stages'].push(job.stage)
+              jobSort[job.stage] = []
+              let jobData = {
+                status: job.status,
+                name: job.name
+              }
+              jobSort[job.stage].push(jobData)
+            }
+          })
+          /* set pipeline stage key */
+          jobSort['pipelineStatus'] = item.jobs[0].pipeline.status
+          item.jobs = jobSort
+        })
+        resolve('true')
+      })
     },
     totalScore: function (jobs) {
       /* check pipeline status */
       if (
-        jobs[0].pipeline.status === 'running' ||
-        jobs[0].pipeline.status === 'pending'
+        jobs.pipelineStatus === 'running' ||
+        jobs.pipelineStatus === 'pending'
       ) {
         return 'running'
       }
       let score = 0
-      jobs.forEach(job => {
-        if (job.status === 'success') {
-          score = score + config.stageScore[job.name]
-        }
+      jobs.stages.forEach(stage => {
+        jobs[stage].forEach(job => {
+          if (job.status === 'success') {
+            score = score + config.stageScore[job.name]
+          }
+        })
       })
       return score
     }
