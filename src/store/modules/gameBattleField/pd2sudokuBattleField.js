d9b1bd0229d333c769a38fbaefbc4b3bfa0f0313
Yao-Hsien Hsieh <laxative0612@gmail.com>
2019-03-27 13:12:28 +0800

    Add pd2sudoku battlefield backend part

--- src/store/modules/gameBattleField/pd2sudokuBattleField.js
+++ src/store/modules/gameBattleField/pd2sudokuBattleField.js
@@ -2,5 +2,67 @@ const axios = require('axios')
 const config = require('../../../../config/setting')
 
 export default {
-  namespaced: true
+  namespaced: true,
+  state: {
+    group: null,
+    process: false
+  },
+  getters: {},
+  mutations: {
+    /**
+     * updating user group
+     *
+     * @param {Object} state - vuex store state
+     * @param {string} group - unregistered, easy, or hard
+     */
+    updateGroup (state, group) { state.group = group }
+  },
+  actions: {
+    /**
+     * getting user's group
+     *
+     * @param {Object} context - vuex store context
+     * @param {string} username - user's gitlab username
+     */
+    getUserGroup (context, username) {
+      axios.get(`${config.hostname}/userGroup?user=${username}`).then(response => {
+        context.commit('updateGroup', response)
+      })
+    },
+    /**
+     * updating user's group
+     *
+     * @param {Object} context - vuex store context
+     * @param {Object} param - contains user and group
+     */
+    updateUserGroup (context, param) {
+      axios.get(`${config.hostname}/updateGroup?user=${param.user}&group=${param.group}`).then(() => { location.reload() })
+    },
+    /**
+     * updating user's judge code version
+     *
+     * @param {Object} context - vuex store context
+     * @param {Object} param - contains userID, username, token, and job id
+     */
+    updateCodeVersion (context, param) {
+      axios.get(`${config.hostname}/artifact?userID=${param.userID}&user=${param.username}&token=${param.token}&jobID=${param.job_id}`).then(() => {
+        alert('update successful')
+      })
+    },
+    /**
+     * request for battle
+     *
+     * @param {Object} context - vuex store context
+     * @param {Object} param - contains user, enemy, and group
+     */
+    battleRequest (context, param) {
+      axios.get(`${config.hostname}/battle?user=${param.user}&enemy=${param.enemy}&group=${param.group}`).then((response => {
+        if (response !== 'done') {
+          /**
+           * TODO: the process response handler
+           */
+        }
+      }))
+    }
+  }
 }
